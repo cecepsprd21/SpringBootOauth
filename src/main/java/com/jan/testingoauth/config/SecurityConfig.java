ae08b59ee2c758fe6ad6e1e6ea4100607bb7b4a3
cecepsprd21 <54131010+cecepsprd21@users.noreply.github.com>
2020-01-06 10:27:22 +0700

    Update SecurityConfig.java

--- src/main/java/com/jan/testingoauth/config/SecurityConfig.java
+++ src/main/java/com/jan/testingoauth/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/authenticate","/user").permitAll()
+                .authorizeRequests().antMatchers("/authenticate").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
